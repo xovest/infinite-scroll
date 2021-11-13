7bc62b6bc0a16ac33d7d902308e385b947d7ec86
xovest <79008917+xovest@users.noreply.github.com>
2021-11-13 23:08:23 +0200

    infscr

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+  APPLICATION_ID: process.env.APPLICATION_ID || 'Txh8ekC_cE2bMdzHY9u63EqkLPqad6W1mLKwb5ih6gc',
+  SECRET: process.env.SECRET || 'XcWypyFbHsNaqoCY8uHf6N3-TJkPqGaYd4gbTZzImCs',
+  CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+};
