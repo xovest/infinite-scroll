b39f93648307bb7ce5af87c693707727d8e1745f
xovest <79008917+xovest@users.noreply.github.com>
2021-11-13 23:17:24 +0200

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="root">
+      <div className="hero is-fullheight is-bold is-info">
+        <div className="hero-body">
+          <div className="container">
+            <div className="header content">
+              <h2 className="subtitle is-6">Code Challenge #16</h2>
+              <h1 className="title is-1">
+                Infinite Scroll Unsplash Code Challenge
+              </h1>
+            </div>
+            <div className="images">
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
