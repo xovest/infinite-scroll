f978088c3da325f4d5f3fe7628c5133ad198aa16
xovest <79008917+xovest@users.noreply.github.com>
2021-11-14 18:21:06 +0200

    got the images

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ const express = require('express');
 const unsplash = new Unsplash({
   applicationId: config.get('APPLICATION_ID'),
   secret: config.get('SECRET'),
-  callbackUrl: config.get('CALLBACK_URL'),
+  callbackUrl: config.get('CALLBACK_URL')
 });
 
 const app = express();
 
 app.get('/api/photos', (req, res) => {
   unsplash.photos
-    .listPhotos(1, 30)
+    .listPhotos(req.query.start, req.query.count)
     .then(toJson)
     .then(json => res.json(json));
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
